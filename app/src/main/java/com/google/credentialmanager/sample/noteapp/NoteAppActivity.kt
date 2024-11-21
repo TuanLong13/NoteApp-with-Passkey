f8f6ff4cdc04bccbd91e5c55aa6abe82b794ed46
HaoPhong11 <nguyenhaophong227@gmail.com>
2024-11-21 09:35:21 +0700

    chinh sua aes va 1 so code

--- app/src/main/java/com/google/credentialmanager/sample/noteapp/NoteAppActivity.kt
+++ app/src/main/java/com/google/credentialmanager/sample/noteapp/NoteAppActivity.kt
@@ -5,7 +5,6 @@ import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.SearchManager
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -15,7 +14,6 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.Toast
